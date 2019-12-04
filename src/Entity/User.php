ea801f8329a9d8e5b4e2c86c853c836fca348ffe
Fabien Stenneler <53312455+fstenneler@users.noreply.github.com>
2019-12-04 16:34:06 +0100

    created user entity

---
+++ src/Entity/User.php
@@ -0,0 +1,177 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Security\Core\User\UserInterface;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\UserRepository")
+ */
+class User implements UserInterface
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=180, unique=true)
+     */
+    private $username;
+
+    /**
+     * @ORM\Column(type="json")
+     */
+    private $roles = [];
+
+    /**
+     * @var string The hashed password
+     * @ORM\Column(type="string")
+     */
+    private $password;
+
+    /**
+     * @ORM\Column(type="string", length=100, nullable=false)
+     */
+    private $company;
+
+    /**
+     * @ORM\Column(type="string", length=100, nullable=false)
+     */
+    private $managerName;
+
+    /**
+     * @ORM\Column(type="string", length=180, nullable=false)
+     */
+    private $managerEmail;
+
+    /**
+     * @ORM\Column(type="string", length=50, nullable=false)
+     */
+    private $managerPhone;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    /**
+     * A visual identifier that represents this user.
+     *
+     * @see UserInterface
+     */
+    public function getUsername(): string
+    {
+        return (string) $this->username;
+    }
+
+    public function setUsername(string $username): self
+    {
+        $this->username = $username;
+
+        return $this;
+    }
+
+    /**
+     * @see UserInterface
+     */
+    public function getRoles(): array
+    {
+        $roles = $this->roles;
+        // guarantee every user at least has ROLE_USER
+        $roles[] = 'ROLE_USER';
+
+        return array_unique($roles);
+    }
+
+    public function setRoles(array $roles): self
+    {
+        $this->roles = $roles;
+
+        return $this;
+    }
+
+    /**
+     * @see UserInterface
+     */
+    public function getPassword(): string
+    {
+        return (string) $this->password;
+    }
+
+    public function setPassword(string $password): self
+    {
+        $this->password = $password;
+
+        return $this;
+    }
+
+    public function getCompany(): ?string
+    {
+        return $this->company;
+    }
+
+    public function setCompany(string $company): self
+    {
+        $this->company = $company;
+
+        return $this;
+    }
+
+    public function getManagerName(): ?string
+    {
+        return $this->managerName;
+    }
+
+    public function setManagerName(string $managerName): self
+    {
+        $this->managerName = $managerName;
+
+        return $this;
+    }
+
+    public function getManagerEmail(): ?string
+    {
+        return $this->managerEmail;
+    }
+
+    public function setManagerEmail(string $managerEmail): self
+    {
+        $this->managerEmail = $managerEmail;
+
+        return $this;
+    }
+
+    public function getManagerPhone(): ?string
+    {
+        return $this->managerPhone;
+    }
+
+    public function setManagerPhone(string $managerPhone): self
+    {
+        $this->managerPhone = $managerPhone;
+
+        return $this;
+    }
+
+    /**
+     * @see UserInterface
+     */
+    public function getSalt()
+    {
+        // not needed when using the "bcrypt" algorithm in security.yaml
+    }
+
+    /**
+     * @see UserInterface
+     */
+    public function eraseCredentials()
+    {
+        // If you store any temporary, sensitive data on the user, clear it here
+        // $this->plainPassword = null;
+    }
+    
+}
--- src/Migrations/Version20191204153330.php
+++ src/Migrations/Version20191204153330.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20191129153643 extends AbstractMigration
+final class Version20191204153330 extends AbstractMigration
 {
     public function getDescription() : string
     {
@@ -26,7 +26,8 @@ final class Version20191129153643 extends AbstractMigration
         $this->addSql('CREATE TABLE category (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(100) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE color (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(100) NOT NULL, hexa VARCHAR(7) DEFAULT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE media (id INT AUTO_INCREMENT NOT NULL, product_id INT NOT NULL, url VARCHAR(255) NOT NULL, INDEX IDX_6A2CA10C4584665A (product_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
-        $this->addSql('CREATE TABLE product (id INT AUTO_INCREMENT NOT NULL, brand_id INT NOT NULL, category_id INT NOT NULL, color_id INT NOT NULL, name VARCHAR(255) NOT NULL, reference VARCHAR(100) NOT NULL, gencode VARCHAR(14) DEFAULT NULL, description LONGTEXT DEFAULT NULL, public_price DOUBLE PRECISION NOT NULL, price DOUBLE PRECISION NOT NULL, stock INT NOT NULL, dimensions VARCHAR(100) DEFAULT NULL, weight VARCHAR(100) DEFAULT NULL, memory VARCHAR(100) DEFAULT NULL, os VARCHAR(100) DEFAULT NULL, display VARCHAR(100) DEFAULT NULL, battery VARCHAR(100) DEFAULT NULL, camera VARCHAR(100) DEFAULT NULL, sensors VARCHAR(100) DEFAULT NULL, connectivity VARCHAR(100) DEFAULT NULL, band_quad VARCHAR(100) DEFAULT NULL, INDEX IDX_D34A04AD44F5D008 (brand_id), INDEX IDX_D34A04AD12469DE2 (category_id), INDEX IDX_D34A04AD7ADA1FB5 (color_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE product (id INT AUTO_INCREMENT NOT NULL, brand_id INT NOT NULL, category_id INT NOT NULL, color_id INT NOT NULL, name VARCHAR(255) NOT NULL, sku VARCHAR(100) NOT NULL, gencode VARCHAR(14) DEFAULT NULL, description LONGTEXT DEFAULT NULL, public_price DOUBLE PRECISION NOT NULL, price DOUBLE PRECISION NOT NULL, stock INT NOT NULL, dimensions VARCHAR(100) DEFAULT NULL, weight VARCHAR(100) DEFAULT NULL, os VARCHAR(100) DEFAULT NULL, display VARCHAR(100) DEFAULT NULL, processor VARCHAR(100) DEFAULT NULL, camera VARCHAR(100) DEFAULT NULL, sensors VARCHAR(100) DEFAULT NULL, INDEX IDX_D34A04AD44F5D008 (brand_id), INDEX IDX_D34A04AD12469DE2 (category_id), INDEX IDX_D34A04AD7ADA1FB5 (color_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE user (id INT AUTO_INCREMENT NOT NULL, username VARCHAR(180) NOT NULL, roles JSON NOT NULL, password VARCHAR(255) NOT NULL, company VARCHAR(100) NOT NULL, manager_name VARCHAR(100) NOT NULL, manager_email VARCHAR(180) NOT NULL, manager_phone VARCHAR(50) NOT NULL, UNIQUE INDEX UNIQ_8D93D649F85E0677 (username), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('ALTER TABLE media ADD CONSTRAINT FK_6A2CA10C4584665A FOREIGN KEY (product_id) REFERENCES product (id)');
         $this->addSql('ALTER TABLE product ADD CONSTRAINT FK_D34A04AD44F5D008 FOREIGN KEY (brand_id) REFERENCES brand (id)');
         $this->addSql('ALTER TABLE product ADD CONSTRAINT FK_D34A04AD12469DE2 FOREIGN KEY (category_id) REFERENCES category (id)');
@@ -47,5 +48,6 @@ final class Version20191129153643 extends AbstractMigration
         $this->addSql('DROP TABLE color');
         $this->addSql('DROP TABLE media');
         $this->addSql('DROP TABLE product');
+        $this->addSql('DROP TABLE user');
     }
 }
